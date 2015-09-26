12e3145c8f0d116b90356811d1ffb8681d1bcd06
sunddenly <1085471904@qq.com>
2015-09-26 23:49:51 +0800

    update

--- WordCount/src/word/WordCount.scala
+++ WordCount/src/word/WordCount.scala
@@ -14,15 +14,19 @@ object WordCount {
     val conf = new SparkConf();
     conf.setAppName("Spark Count")
     conf.setMaster("local")
+    val inpath="file:///F:/Project/SparkProject/SparkProject/WordCount/file.txt"
+    val outpath="file:///F:/Project/SparkProject/SparkProject/WordCount/out"
+    System.setProperty("hadoop.home.dir", "F:/Project/SparkProject/hadoop-2.6.0/hadoop-2.6.0")
     val sc = new SparkContext(conf)
     // split each document into words
-    val tokenized = sc.textFile("file:///F:/Project/SparkProject/WorkSpace/WordCount/file.txt").flatMap(_.split(" "))
+    val tokenized = sc.textFile(inpath).flatMap(_.split(" "))
     // count the occurrence of each word
     val wordCounts = tokenized.map((_, 1)).reduceByKey(_ + _)
-    // filter out words with less than threshold occurrences
-    val filtered = wordCounts.filter(_._2 >= threshold)
-    // count characters
-    val charCounts = filtered.flatMap(_._1.toCharArray).map((_, 1)).reduceByKey(_ + _)
-    System.out.println(wordCounts.collect().mkString(", "))
+    wordCounts.saveAsTextFile(outpath)
+//    filter out words with less than threshold occurrences
+//    val filtered = wordCounts.filter(_._2 >= threshold)
+//    // count characters
+//    val charCounts = filtered.flatMap(_._1.toCharArray).map((_, 1)).reduceByKey(_ + _)
+//    System.out.println(wordCounts.collect().mkString(", "))
   }
 }
